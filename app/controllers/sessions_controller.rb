61e8130e6f83e565a7473ce95f438219dab2083b
YanC Lin <yonalin1@gmail.com>
2018-09-24 20:17:13 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
      gits = GithubService.new
      session[:token] = gits.authenticate!(ENV["GITHUB_CLIENT"], ENV["GITHUB_SECRET"], params[:code])
      session[:username] = gits.get_username
- 
+
      redirect_to '/'
    end
  end
