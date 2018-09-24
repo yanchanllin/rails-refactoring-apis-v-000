b0d85ca9d437985b73821fb052a2a858764ae8b5
YanC Lin <yonalin1@gmail.com>
2018-09-24 20:15:05 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,7 +2,7 @@ class RepositoriesController < ApplicationController
   def index
      @repos_array = GithubService.new({"access_token" => session[:token]}).get_repos
    end
- 
+
    def create
      GithubService.new({"access_token" => session[:token]}).create_repo(params[:name])
      redirect_to '/'
